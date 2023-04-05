2f84e068f544560c67ae5540e21f69ecbdea7ad6
Tomasz Bryndzia <tomasz.bryndzia@justrelate.com>
2023-04-05 09:31:31 +0200

    Suggestions

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Input = ({ ...props }) => {
+const Input = (props) => {
   return (
     <input
       type="text"
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,9 @@
 const Navbar = () => {
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
-                <h1 className="text-white">Contacts</h1>
-            </nav>
-        </>)
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
+            <h1 className="text-white">Contacts</h1>
+        </nav>
+    )
 };
 
 export default Navbar;
